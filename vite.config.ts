fc22a3ccd71666128a4b230d00f7f0ce806b13e5
NoNiQu <marcos.workstation@gmail.com>
2025-05-26 22:39:21 +0200

    arruiab

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { vercelPreset } from "@react-router/dev";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  ...vercelPreset()
 });
